c5f3ed0cfeb7d1b62fdd1a726254dae4c7e4636e
Vlad Step <sobol_v92@mail.ru>
2024-06-24 21:24:17 +0300

    add comments

--- src/components/page-authorization.component.js
+++ src/components/page-authorization.component.js
@@ -2,14 +2,18 @@ import { Component } from "../core/component.js";
 
 export class PageAuthorization extends Component {
     constructor(id) {
+        // прокидываем id в constructor родителя
         super(id)
 
     }
 
     init() {
+        // инициализируем формы
         this.signIn = new Component('sign-in')
         this.signUp = new Component('sign-up')
+            // получаем ссылки
         this.links = this.componet.querySelectorAll('.form__link')
+            // навешиваем слушатели событий
         this.links.forEach(link => {
             link.addEventListener('click', onChangeFormHandler.bind(this))
 
@@ -20,6 +24,7 @@ export class PageAuthorization extends Component {
 
 
 function onChangeFormHandler(event) {
+    // функция переключения форм с Sign-in на Sign-up и обратно
     event.preventDefault();
     if (event.target.classList.contains('link-in')) {
         this.signUp.hide();
--- src/core/component.js
+++ src/core/component.js
@@ -5,13 +5,17 @@
          this.init()
      }
 
-     init() {}
+     init() {
+         // заглушка чтобы не было ошибки при вызове this.init() (если вдруг этого метода не будет в наследниках)
+     }
 
      hide() {
+         // метод для сокрытия полученного элемента (this.componet)
          this.componet.classList.add('hide')
      }
 
      show() {
+         // метод для отображения полученного элемента (this.componet)
          this.componet.classList.remove('hide')
      }
 
