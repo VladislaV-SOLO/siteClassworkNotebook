669db342216f219977eedebfc65f6e4a59961fcb
Vlad Step <sobol_v92@mail.ru>
2024-07-08 21:24:16 +0300

    lesson 6.1

--- src/core/component.js
+++ src/core/component.js
@@ -13,15 +13,20 @@
          // заглушка чтобы не было ошибки при вызове this.onHide() (если вдруг этого метода не будет в наследниках)
      }
 
+     onShow() {
+         // заглушка чтобы не было ошибки при вызове this.onShow() (если вдруг этого метода не будет в наследниках)
+     }
+
      hide() {
          // метод для сокрытия полученного элемента (this.componet)
          this.componet.classList.add('hide')
          this.onHide()
      }
 
-     show() {
+     show(e) {
          // метод для отображения полученного элемента (this.componet)
          this.componet.classList.remove('hide')
+         this.onShow(e)
      }
 
 
