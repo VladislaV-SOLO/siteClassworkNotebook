e3f596e9ebb117b047dff4bc38b4872d02d4d3fc
Vlad Step <sobol_v92@mail.ru>
2024-07-18 21:27:04 +0300

    lesson 8

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import { Notification } from "./components/modals/notification.component.js";
 
 
 
-
-
 const loginPage = new PageAuthorization('login')
 export const notification = new Notification('notification')
